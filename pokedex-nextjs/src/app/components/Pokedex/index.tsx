eb3668f25ef43ab38829b615dc5463266b5203af
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2024-07-23 22:51:24 -0400

    update class to className — typescript is garbo

--- pokedex-nextjs/src/app/components/Pokedex/index.tsx
+++ pokedex-nextjs/src/app/components/Pokedex/index.tsx
@@ -1,60 +1,58 @@
 import styles from '../PokemonDisplay.module.scss'
+import Image from 'next/image'
 
 export default function Pokedex() {
   return (
     <div className={styles.pokedex}>
-      {
-         <div id="pokedex">
-         <div class="sensor">
-           <button></button>
-         </div>
-         <div class="camera-display">
-           <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png"/>
-         </div>
-         <div class="divider"></div>
-         <div class="stats-display">
-           <h2>Charmander</h2>
-           <h3>Abilities</h3>
-           <ul>
-             <li>Solar-power</li>
-             <li>Blaze</li>
-           </ul>
-           <h3>Moves</h3>
-           <ul>
-             <li>dragon-rage</li>
-             <li>dragon-breath</li>
-             <li>dragon-claw</li>
-           </ul>
-         </div>
-         <div class="botom-actions">
-           <div id="actions">
-             <button class="a"></button>
-           </div>
-           <div id="cross">
-             <button class="cross-button up"></button>
-             <button class="cross-button right"></button>
-             <button class="cross-button down"></button>
-             <button class="cross-button left"></button>
-             <div class="cross-button center"> </div>
-           </div>
-         </div>
-         <div class="input-pad"><input /></div>
-       
-         <div class="bottom-modes">
-           
-             <button class="level-button"></button>
-             <button class="level-button"></button>
-             <button class="level-button"></button>
-             <button class="level-button"></button>
-           
-             <button class="pokedex-mode black-button">Pokedex</button>
-             <button class="game-mode black-button">Game</button>
-           
-         </div>
-       
-       </div>
-
-      }
+      <div id="pokedex">
+        <div className="sensor">
+          <button></button>
+        </div>
+        <div className="camera-display">
+          <Image 
+            src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png"
+            alt="Charmander"
+            width={300}
+            height={300}
+          />
+        </div>
+        <div className="divider"></div>
+        <div className="stats-display">
+          <h2>Charmander</h2>
+          <h3>Abilities</h3>
+          <ul>
+            <li>Solar-power</li>
+            <li>Blaze</li>
+          </ul>
+          <h3>Moves</h3>
+          <ul>
+            <li>dragon-rage</li>
+            <li>dragon-breath</li>
+            <li>dragon-claw</li>
+          </ul>
+        </div>
+        <div className="botom-actions">
+          <div id="actions">
+            <button className="a"></button>
+          </div>
+          <div id="cross">
+            <button className="cross-button up"></button>
+            <button className="cross-button right"></button>
+            <button className="cross-button down"></button>
+            <button className="cross-button left"></button>
+            <div className="cross-button center"> </div>
+          </div>
+        </div>
+        <div className="input-pad"><input /></div>
+        <div className="bottom-modes">
+          <button className="level-button"></button>
+          <button className="level-button"></button>
+          <button className="level-button"></button>
+          <button className="level-button"></button>
+          <button className="pokedex-mode black-button">Pokedex</button>
+          <button className="game-mode black-button">Game</button>
+        </div>
+      </div>
     </div>
   )
 }
