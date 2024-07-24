790f4ebc10cc9fe84e19e1146fbf033e4f8f9d60
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2024-07-23 20:31:33 -0400

    update convex part deux

--- pokedex-nextjs/convex/_generated/api.d.ts
+++ pokedex-nextjs/convex/_generated/api.d.ts
@@ -14,6 +14,8 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as addScore from "../addScore.js";
+import type * as getTopScores from "../getTopScores.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -23,7 +25,10 @@ import type {
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  addScore: typeof addScore;
+  getTopScores: typeof getTopScores;
+}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
